063d59ead56bb62a91f0ff681c22a5709d2ea8de
Jezzamonn <jezzamonn@gmail.com>
2016-01-29 22:44:50 +1100

    initial commit

---
+++ Main.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using Assets.code;
+
+public class Main : MonoBehaviour {
+
+    public List<TrailPoint> trail;
+
+	// Use this for initialization
+	void Start () {
+        trail = new List<TrailPoint>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    
+	}
+}
---
+++ code/TrailPoint.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Assets.code
+{
+
+    public class TrailPoint
+    {
+        public Vector2 Pos { get; set; }
+
+        public Vector2 Dir { get; set; }
+    }
+}
