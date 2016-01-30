73e711f6d04be982d58fe45e8be7a20d60853a57
Jezzamonn <jezzamonn@gmail.com>
2016-01-30 23:27:15 +1100

    Got the queen in it

---
+++ Assets/code/Charger.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+namespace Assets.code
+{
+    public class Charger : MonoBehaviour
+    {
+
+        public enum State
+        {
+            Idle,
+            Agro
+        }
+
+        public State state = State.Idle;
+        public float agroDist = 10;
+
+        public void TryAgro(Player player)
+        {
+            float dist = Vector3.Distance(player.transform.position, transform.position);
+            if (dist < agroDist)
+            {
+                state = State.Agro;
+            }
+        }
+
+
+    }
+}
